181aded94e58001495dad9f55519d0a4251d6fb9
Suni-dot-ui <Sanjanakoli2926@gmail.com>
2026-04-27 19:16:38 -0400

    update

--- src/screens/auth/login.js
+++ src/screens/auth/login.js
@@ -39,21 +39,3 @@ export default function Login() {
     </div>
   );
 }
-
-
-/**import React from "react";
-import { redirectToLogin } from '../../spotify';
-import "./login.css";
-
-export default function Login() {
-  return (
-    <div className="login-page">
-       <img 
-        src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_White.png"
-        alt="logo-spotify"
-        className="logo" 
-        />
-          <div className="login-btn" onClick={redirectToLogin}>LOG IN</div>
-    </div>
-  );
-}*/
